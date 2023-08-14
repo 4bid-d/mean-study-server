d86590b54c2b8b617036e941da2ef75bb624c782
4bid-d <abidpponline@gmail.com>
2023-08-15 02:21:49 +0530

    added togglelable sidebar

--- client/src/Components/general/navbar/navbar.jsx
+++ client/src/Components/general/navbar/navbar.jsx
@@ -2,13 +2,21 @@ import React, { useContext } from 'react'
 import { adminKeyContext } from '../../../Hooks/useAdminKey'
 import "./navbar.css" 
 
-function MainNavbar({head , profile }) {
+function MainNavbar({head , profile ,sidebarToggle}) {
   let adminSecret  = useContext(adminKeyContext)
   console.log(adminSecret ? adminSecret.key :"not an admin" )
+  function side (){
+      let sidebar = document.querySelector(".sidebar-wrapper")
+      let navbar= document.querySelector(".navbar")
+      sidebar.classList.toggle("off")
+      navbar.classList.toggle("short")        
+  }
   return (
     <>
-     <nav className="navbar row">
+     <nav className="navbar short row">
+          {/* <i className="fa-solid fa-sidebar" style={{color: "#ffff"}}></i> */}
             <div className="nav-title col-lg-2 col-md-4 col-sm-12 ">
+              <span class="material-symbols-outlined" onClick={side}>thumbnail_bar</span>
               {/* <a href="#" className="toggle-button ">
                   <span className="bar"></span>
                   <span className="bar"></span>

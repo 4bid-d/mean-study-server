11540dd667acb740e7d2d9b97338628fd4472dc4
4bid-d <abidpponline@gmail.com>
2023-08-15 02:17:48 +0530

    styled login page

--- client/src/Components/auth/LoginForm/Login.js
+++ client/src/Components/auth/LoginForm/Login.js
@@ -3,16 +3,22 @@ import React , { useRef, useState } from 'react'
 import Loading from "../../Loading/Loading"
 import {useNavigate} from "react-router-dom"
 import {FetchRequest, UseFetch} from "../../../Hooks/useFetch"
-import {setLocalstorage} from "../../../Hooks/useLocalstorage"
+import {setLocalstorage,getLocalstorage} from "../../../Hooks/useLocalstorage"
 import {
   VALIDATION_MESSAGES,
 }  from "../../../config/formValidation"
+import { useEffect } from "react"
 function Login() { 
   const [loading , setLoading] = useState(false)
   const password =  useRef()
   const username =  useRef()
   const email =  useRef()
   const navigate = useNavigate()
+  useEffect(()=>{
+    let data  = getLocalstorage("Token")
+    if(data ) navigate("/")
+    console.log(data)
+  })
   const sendLoginData = (e)=>{
     e.preventDefault()
     const currentUsername =  username.current.value
@@ -27,8 +33,9 @@ function Login() {
         password : currentPassword,   
       }).then((result)=>{
         if(!result) throw new Error(VALIDATION_MESSAGES.BASIC.SOMETHING_WRONG)
-        if(result.error)  throw new Error(result.error)
-         alert(result.message)
+        if(result.error){
+          alert(result.error)
+        }
          if(result.token){
            setLocalstorage("Token","")
            setLocalstorage('Token', result.token)
@@ -40,22 +47,31 @@ function Login() {
   if(loading) return <Loading/>
   return (
     <>
-      <div className="login-form-div">
-          <h4>Login Form</h4>
-        <form >
-          <input type="text" ref={username} placeholder="User name" />
-          <input type="email" ref={email} placeholder="Email" />
-          <input type="password" ref={password} placeholder="Password" />
-          <button type="submit" onClick={
-            (e)=>{
-              try {
-                sendLoginData(e) 
-              } catch (error) {
-                alert(error.message)
-              }
-            }}>Submit</button>
-        </form>
-      </div>
+      <div className="form-wrapper">
+          <div className="login-form-div container">
+              <div className="head-wrapper">
+                <h4>Login Form</h4>
+              </div>
+            <form >
+              <div className="user-email">
+                <input className=" input-username" type="text" ref={username} placeholder="User name" />
+                <input  className="input-email" type="email" ref={email} placeholder="Email" />
+              </div>
+              <div>
+                <input className="input-password" style={{color:"green"}} type="password" ref={password} placeholder="Password" />
+              </div>
+                  <button className="button-login"   type="submit" onClick={
+                    (e)=>{
+                      try {
+                        sendLoginData(e) 
+                      } catch (error) {
+                        alert(error.message)
+                      }
+                    }}>Submit</button>
+      
+            </form>
+          </div>
+        </div>
     </>
 
   )
